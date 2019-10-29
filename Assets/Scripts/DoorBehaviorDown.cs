147733cb97db37651bc78b182f9311260a76c7f6
Ulleseit <ulleseit99@gmail.com>
2019-10-28 23:08:31 -0700

    Door and Camera Behavior
    Created 3 rooms in sample scene, camera moves between rooms as previously discussed, doors have also been implemented as part of this.

---
+++ Assets/Scripts/DoorBehaviorDown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//For comments please see DoorBehaviorRight
+
+public class DoorBehaviorDown : MonoBehaviour
+{
+	GameObject MainCamera;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+       MainCamera = GameObject.Find("Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+	
+		if(other.gameObject.name == "Player")
+		{
+				other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y-12, 0);
+				MainCamera.transform.Translate(0,-26,0);
+		}
+	
+	}
+}
---
+++ Assets/Scripts/DoorBehaviorRight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DoorBehaviorRight : MonoBehaviour
+{
+	GameObject MainCamera;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+       MainCamera = GameObject.Find("Camera");//Gets Camera Object from scene
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerEnter2D(Collider2D other)//When object that the script is on is collided with
+	{
+	
+		if(other.gameObject.name == "Player")//Activate only for player collision
+		{
+				other.transform.position = new Vector3(other.transform.position.x+12, other.transform.position.y, 0);//Moves the player 12 units to the right
+				MainCamera.transform.Translate(29,0,0);//Moves camera 29 units to the right
+		}
+	
+	}
+}
