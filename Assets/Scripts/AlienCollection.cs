dc9d09b10266c2b8ca281f628ef944c99fdc7ab7
usaruner <usaruner@gmail.com>
2019-11-03 19:59:26 -0800

    Finshed Alien Collector
    aliens appear
    allowes human player to collect alien
    alien counter of alien collected

---
+++ Assets/Scripts/AlienCollection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AlienCollection : MonoBehaviour
+{
+	public int Aliens; //Number of Aliens Collected
+	GameObject alienCollection; // Game Object to find AlienCollection
+	Text text; // Text
+	
+    void Start()
+    {
+    	alienCollection = GameObject.Find("Canvas/AlienCollection"); //find AlienCollection Script
+	text = alienCollection.GetComponent<Text>(); //get text component set to text
+    }
+
+    void Update()
+    {
+	text.text = "Aliens Collected: " + Aliens; // update text;
+    }
+    public void Collect(){
+    	Aliens++; // Increment Alien Collected
+    }
+}
---
+++ Assets/Scripts/CollectAlien.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectAlien: MonoBehaviour
+{
+    GameObject AlienSprite; // used to find AlienCollection
+    AlienCollection alienCollection; // used to get AlienCollection
+    // Start is called before the first frame update
+    void Start()
+    {
+    	
+        AlienSprite = GameObject.Find("Canvas/AlienCollection"); // Find AlienCollection
+        alienCollection = AlienSprite.GetComponent<AlienCollection>(); // Set alienCollection to the found gameObject
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnTriggerEnter2D(Collider2D other){
+     	if(other.gameObject.name == "Player"){ // Only Player
+     		alienCollection.Collect(); // Incriment Alien
+     		Destroy(gameObject); // Remove current Object 
+     	}
+     }
+    
+}
