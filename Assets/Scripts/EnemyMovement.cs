38acbef720ecf62751b9e4cbc89e6ab21a9c48f7
Victoria W <56736494+Tori-W@users.noreply.github.com>
2019-11-07 09:54:57 -0800

    EnemyMovement completed.
    Enemy now moves toward the player constantly.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 playerPos;
+    public Vector3 enemyPos;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = GameObject.Find("Player").transform.position;
+        enemyPos = this.transform.position;
+        this.transform.position = Vector3.MoveTowards(enemyPos, playerPos, 0.02f); //Adjust float as needed. Higher numbers = faster
+    }
+}
