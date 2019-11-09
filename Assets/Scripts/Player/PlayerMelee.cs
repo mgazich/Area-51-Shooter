39279aad36ae84297f671befb37cab494f802491
Henry Feng <usaruner@gmail.com>
2019-11-09 09:58:16 -0800

    PlayerMelee attack
    player melee Attack mapped to spacebar

---
+++ Assets/Scripts/Player/PlayerMelee.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMelee : MonoBehaviour
+{
+        public GameObject attack; //for image
+        public GameObject enemy; //player
+        public GameObject weaponSwap;
+        public float attackRange; //changeable range
+        // Start is called before the first frame update
+        void Start()
+        {
+        weaponSwap =GameObject.Find("WeaponSwap");
+    	enemy = GameObject.Find("Enemy"); //find player
+    	attack = GameObject.Find("PlayerMelee"); //find this
+    	attackRange = 2; //range intitally 2
+    	attack.SetActive(false); // disappear
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {	
+        	//if distance < range
+		if(Input.GetKeyDown("space")){
+			StartCoroutine( ShowAndHide(attack, 0.5f) );	
+		}
+    	}
+    	IEnumerator ShowAndHide( GameObject go, float delay )
+	{
+	    go.SetActive(true);
+	    yield return new WaitForSeconds(delay);
+	    go.SetActive(false);
+        }
+}
---
+++ Assets/Scripts/Player/WeaponSwap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwap : MonoBehaviour
+{
+    public Weapons weapon;
+    public enum Weapons{Ranged, Melee}
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        weapon = Weapons.Ranged;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
