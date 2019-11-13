20152abcf76720f95efa08d67cc1f7ffbf6f6d8d
Ulleseit <ulleseit99@gmail.com>
2019-11-12 17:47:14 -0800

    Shuriken Throwing
    Implemented shuriken throwing, shuriken is destroyed upon contact with enemy or outer wall. Need a way to destroy when in contact with inner walls, as well as need to fix velocity of shuriken depending on where it is clicked.

--- Assets/Scripts/Player/PlayerMelee.cs
+++ Assets/Scripts/Player/PlayerMelee.cs
@@ -11,25 +11,26 @@ public class PlayerMelee : MonoBehaviour
         // Start is called before the first frame update
         void Start()
         {
-        weaponSwap =GameObject.Find("WeaponSwap");
-    	enemy = GameObject.Find("Enemy"); //find player
-    	attack = GameObject.Find("PlayerMelee"); //find this
-    	attackRange = 2; //range intitally 2
-    	attack.SetActive(false); // disappear
+			weaponSwap =GameObject.Find("WeaponSwap");
+			enemy = GameObject.Find("Enemy"); //find player
+			attack = GameObject.Find("PlayerMelee"); //find this
+			attackRange = 2; //range intitally 2
+			attack.SetActive(false); // disappear
         }
     
         // Update is called once per frame
         void Update()
         {	
-        	//if distance < range
-		if(Input.GetKeyDown("space")){
-			StartCoroutine( ShowAndHide(attack, 0.5f) );	
-		}
+				//if distance < range
+			if(Input.GetKeyDown("space"))
+			{
+				StartCoroutine( ShowAndHide(attack, 0.5f) );	
+			}
     	}
-    	IEnumerator ShowAndHide( GameObject go, float delay )
-	{
-	    go.SetActive(true);
-	    yield return new WaitForSeconds(delay);
-	    go.SetActive(false);
-        }
+		IEnumerator ShowAndHide( GameObject go, float delay )
+		{
+			go.SetActive(true);
+			yield return new WaitForSeconds(delay);
+			go.SetActive(false);
+		 }
 }
---
+++ Assets/Scripts/ShurikenHit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShurikenHit : MonoBehaviour
+{
+	public float damageAmount = 1f;
+	GameObject MainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = GameObject.Find("Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if((transform.position.x > (MainCamera.transform.position.x)+12) || (transform.position.x < (MainCamera.transform.position.x)-12) || (transform.position.y > (MainCamera.transform.position.y)+10) || (transform.position.y < (MainCamera.transform.position.y)-10))
+		{
+			Debug.Log(transform.position.x + " : " + MainCamera.transform.position.x);
+			Destroy(gameObject);
+		}	
+	}
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+	    if (collision.CompareTag("Enemy"))
+	    {
+	    	Enemy enemy = collision.GetComponent<Enemy>();
+	    	enemy.Damage(damageAmount);
+			Destroy(gameObject);
+	    }
+		
+    }
+}
