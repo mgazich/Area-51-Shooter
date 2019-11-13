78bbdf50eb8e0153e54f5c015f4ed33ba127ce02
mgazich <50192506+mgazich@users.noreply.github.com>
2019-11-12 21:20:06 -0800

    Fixed Shuriken Wall Collision
    Stopped the shuriken from going through the walls.

--- Assets/Scripts/ShurikenHit.cs
+++ Assets/Scripts/ShurikenHit.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ShurikenHit : MonoBehaviour
 {
 	public float damageAmount = 1f;
+    public LayerMask collisionLayers;
 	GameObject MainCamera;
     // Start is called before the first frame update
     void Start()
@@ -15,20 +16,34 @@ public class ShurikenHit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if((transform.position.x > (MainCamera.transform.position.x)+12) || (transform.position.x < (MainCamera.transform.position.x)-12) || (transform.position.y > (MainCamera.transform.position.y)+10) || (transform.position.y < (MainCamera.transform.position.y)-10))
+		/*if((transform.position.x > (MainCamera.transform.position.x)+12) || (transform.position.x < (MainCamera.transform.position.x)-12) || (transform.position.y > (MainCamera.transform.position.y)+10) || (transform.position.y < (MainCamera.transform.position.y)-10))
 		{
 			Debug.Log(transform.position.x + " : " + MainCamera.transform.position.x);
 			Destroy(gameObject);
-		}	
+		}	*/
 	}
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-	    if (collision.CompareTag("Enemy"))
+        
+        if (collision.CompareTag("Enemy"))
 	    {
 	    	Enemy enemy = collision.GetComponent<Enemy>();
 	    	enemy.Damage(damageAmount);
 			Destroy(gameObject);
 	    }
-		
+
+        if (1 << collision.gameObject.layer == collisionLayers.value)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       // Debug.Log("Hello");
+        if (collision.gameObject.layer == collisionLayers)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
