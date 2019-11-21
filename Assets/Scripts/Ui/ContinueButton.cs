d7d71a4d3492a1e59d47968dcba9c28dce0e897a
Victoria W <56736494+Tori-W@users.noreply.github.com>
2019-11-21 12:24:37 -0800

    Pause Menu Added
    - Use ESC key to pause
    - Pause works by setting the timescale to 0. All actions that are affected by framerate/timescale are paused (player movement, enemy movement, timer, etc)

---
+++ Assets/Scripts/Ui/ContinueButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ContinueButton : MonoBehaviour
+{
+    GameObject Player;
+    PauseGame pauseGame;
+
+    private void Start()
+    {
+        Player = GameObject.Find("Player");
+        pauseGame = Player.GetComponent<PauseGame>();
+    }
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pauseGame.unpauseGame();
+
+    }
+}
---
+++ Assets/Scripts/Ui/MenuButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
+}
