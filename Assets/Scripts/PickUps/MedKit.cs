9ac65420e979e953ce789d83ffd7ad8a331e9618
Victoria W <56736494+Tori-W@users.noreply.github.com>
2019-11-26 09:44:51 -0800

    Added MedKit
    - MedKit pick-up added
    - MedKit restores player health for a set amount

---
+++ Assets/Scripts/PickUps/MedKit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedKit : MonoBehaviour
+{
+    GameObject player;
+    PlayerHealth playerHealth;
+    public float healAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            playerHealth.HealDamage(healAmount);
+            Destroy(gameObject); // Remove current Object
+        }
+    }
+}
