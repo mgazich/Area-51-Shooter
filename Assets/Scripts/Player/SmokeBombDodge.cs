ae57ea4e9cb0f33992ac0d4f6142151e49d4ecd6
Ulleseit <ulleseit99@gmail.com>
2019-11-28 01:59:32 -0800

    Smoke Bomb Dodge Fix
    Cannot teleport through and into walls anymore, should not be any more issues with smoke bomb dodge.

---
+++ Assets/Scripts/Player/SmokeBombDodge.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SmokeBombDodge : MonoBehaviour
+{
+	private Camera cam;
+	private Vector3 teleportDirection;
+	private float teleportRate = 2f;
+	public float nextTeleport = 0f;
+	public Tilemap walls;
+	Vector3Int v3int;
+	PlayerMovement playerMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+       cam = Camera.main;
+	   playerMovement = gameObject.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 v3 = (cam.ScreenToWorldPoint(Input.mousePosition));
+		v3int = walls.WorldToCell(v3);
+		if (Input.GetButton ("Fire2") && Time.time > nextTeleport && walls.GetTile(v3int) == null)
+		{
+			nextTeleport = Time.time + teleportRate;
+			teleportDirection = cam.ScreenToWorldPoint(Input.mousePosition);
+			teleportDirection.z = 0.0f;
+			this.transform.position = (teleportDirection);
+			StartCoroutine(ExampleCoroutine());
+		}
+		
+    }
+	
+	IEnumerator ExampleCoroutine()
+	{
+		float temp = playerMovement.speed;
+		playerMovement.speed = 0.0f;
+		yield return new WaitForSeconds(.001f);
+		playerMovement.speed = temp;
+	}
+	
+
+	
+}
