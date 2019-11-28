9f8caffe7d9599e6042aee7e6699366440e6c31a
Victoria W <56736494+Tori-W@users.noreply.github.com>
2019-11-27 23:26:48 -0800

    Credits Added
    - Producer added
    - Programmers added
    - Credits Button is now functional

---
+++ Assets/Scripts/Ui/CreditsButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsButton : MonoBehaviour
+{
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("CreditsScreen");
+    }
+
+}
