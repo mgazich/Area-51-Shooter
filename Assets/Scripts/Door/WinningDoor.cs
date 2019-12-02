0a56b7a0383e620e3cd0171cdffcab2425a9f71a
Ulleseit <ulleseit99@gmail.com>
2019-12-02 15:28:45 -0800

    Added a bonus score for how much time you escape with
    Time bonus is 5 score for each second

--- Assets/Scripts/Door/WinningDoor.cs
+++ Assets/Scripts/Door/WinningDoor.cs
@@ -5,22 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class WinningDoor : MonoBehaviour
 {
+    Score s;
+    GameObject timer;
+    TimerText timerText;
     // Start is called before the first frame update
     void Start()
     {
-        
+      timer = GameObject.Find("Canvas/Timer");
+      timerText = timer.GetComponent<TimerText>();
+      s = Score.instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-	
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.gameObject.name == "Player")
 		{
+      s.score += (int)(timerText.seconds*5 + (timerText.minutes*60)*5);
 			SceneManager.LoadScene("EscapeScreen");
 		}
 
