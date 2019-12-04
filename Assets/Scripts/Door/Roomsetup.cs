46de63a7e66a1a285a5d7fe3d3796a9fecbb078f
Ulleseit <ulleseit99@gmail.com>
2019-12-03 18:54:19 -0800

    Fixed a bugged room
    Enemy spawned in wall in one corridor, fixed by not spawning enemies in that room.

--- Assets/Scripts/Door/Roomsetup.cs
+++ Assets/Scripts/Door/Roomsetup.cs
@@ -10,15 +10,16 @@ public class Roomsetup : MonoBehaviour
 	// 3 = spawn + map 3 entrance
 	// 4 = coordor down the line
 	// x = NULL
+	// B = Broken
 	//
 	// 5| x x x x x x
 	// 4| x 1 1 1 x x 1
-	// 3| x 1 2 4 3 x 2 
+	// 3| x 1 2 4 B x 2
 	// 2| x 2 3 4 3 4 2
 	// 1| 1 1 x 1 x s 1 1
-	// 0| x 2 1 1 1 s x 
+	// 0| x 2 1 1 1 s x
 	// y ___________________
-	//  x 0 1 2 3 4 5 6 7 
+	//  x 0 1 2 3 4 5 6 7
 	//
 	//
 	GameObject MainCamera;
@@ -33,13 +34,13 @@ public class Roomsetup : MonoBehaviour
      		SpawnGrid = new int[8,6];
 		SpawnGrid[0,1] = SpawnGrid[1,1] = SpawnGrid[1,3] = SpawnGrid[1,4] = SpawnGrid[2,0] = SpawnGrid[2,4] = SpawnGrid[3,0] = SpawnGrid[3,4] = SpawnGrid[4,0] = SpawnGrid[6,1] = SpawnGrid[6,4] =  SpawnGrid[7,1] = SpawnGrid[3,1] = 1;
 		SpawnGrid[1,2] = SpawnGrid[1,0] = SpawnGrid[2,3] = SpawnGrid[6,2] = SpawnGrid[6,3] = SpawnGrid[5,0] = SpawnGrid[5,1] =2;
-		SpawnGrid[2,2] = SpawnGrid[4,2] = SpawnGrid[4,3] = 3;
+		SpawnGrid[2,2] = SpawnGrid[4,2] = 3;
 		SpawnGrid[3,2] = SpawnGrid[3,3] = SpawnGrid[5,2] = 4;
      	}
-	
+
 
 	public void spawnBasedOf(int x,int y){
-		//Basic Square Room	
+		//Basic Square Room
 		int range = 8;
 		if(SpawnGrid[x,y] == 1){
 			Enemy1 = EnemyPooler.SharedInstance.GetPooledEnemy();
@@ -91,7 +92,7 @@ public class Roomsetup : MonoBehaviour
 			Enemy2.transform.position = new Vector3(MainCamera.transform.position.x + range,MainCamera.transform.position.y - range,0);
 					}
 			SpawnGrid[x,y] = -1;
-		
+
 		}
 		if(SpawnGrid[x,y] == 4){
 		range = 5;
